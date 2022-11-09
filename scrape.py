bbb854145ea8bbc37ab7930d1f4ec82625673388
nikhilNathwani <nnathwani36@gmail.com>
2022-11-09 14:59:16 -0800

    Removed unused imports

--- scrape.py
+++ scrape.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
+#Scrape results from 2021-2022 regular season
+#Output should be a list of dicts containing: 
+#	"team": <team name (str)>
+#   "date": <date of game (datetime)>
+#   "outcome": <1 if team won, 0 if lost (int/bool)>
+#   "odds": <moneyline (int)> 
 
 
 #Configure webdriver 
@@ -15,24 +19,36 @@ print("Configured webdriver...")
 #^consider efficiency improvements e.g. add options for not loading images     
 
 
-#Scrape results from 2021-2022 regular season
-#Output should be a list of dicts containing: 
-#	"team": <team name (str)>
-#   "date": <date of game (datetime)>
-#   "outcome": <1 if team won, 0 if lost (int/bool)>
-#   "odds": <moneyline (int)>   
-
-
 def makeSoup(url):
 	#Get page contents
 	driver.get(url)
 	html= driver.page_source
 	return BeautifulSoup(html,'lxml')
 
-def getGameData(soup):
+
+#Leaving date aside for now
+def getAllGameData(soup):
 	table= soup.find("table", "table-main")
 	rows= table.find_all("tr", class_="deactivate")
+	for row in rows: 
+		print(row.prettify())
+		print("\n\n\n")
+		scrapeGame(row)
 	return rows
+
+
+#Given a table row (which corresponds to an NBA game), 
+#this function returns a list [a,b], where 'a' and 'b' 
+#are game dicts for the 2 teams that played in that game.
+#!Currently doesn't record the date of the game yet!
+def scrapeGame(row):
+	teams= row.find("td", "table-participant")
+
+
+
+
+
+
 
 if __name__ == '__main__':
 	print("Making soup...")
@@ -41,6 +57,6 @@ if __name__ == '__main__':
 
 	print("Getting table data...")
 	games= getGameData(soup)
-	print(games)
+	#print(games)
 
 	driver.quit()
