a58aed1df89c76ce4b77261e79d8794f55d822bc
nikhilNathwani <nnathwani36@gmail.com>
2022-11-29 15:04:45 -0800

    Added helper funtions into database.py that convert Game objects into lists of their attributes, so that while I'm scraping I can easily insert rows into my database

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from Game import game
 
 con = sqlite3.connect("moneyline.db")
 cur = con.cursor()
@@ -6,6 +7,18 @@ cur = con.cursor()
 def createGamesTable():
 	#Specifying data types is optional. Can add later.
 	cur.execute("CREATE TABLE games(team, seasonStartYear, gameNumber, outcome, winOdds, loseOdds)")
+
+#Converts Game object into a list of its attributes, in the appopriate
+#order for getting inserted into the 'games' table of moneyline.db
+def listifyGame(game):
+	return [game.team, game.seasonStartYear, game.gameNumber, game.outcome, game.winOdds, game.loseOdds]
+
+#Same as listifyGame() but for a list of multiple Game objects
+def listifyGames(gameList):
+	result=[]
+	for game in gameList:
+		result += listifyGame(game)
+	return result
 
 if __name__ == '__main__':
 	#createGamesTable()
