9f97e273abaadb4adfd288e57a5cbdc539dbe283
nikhilNathwani <nnathwani36@gmail.com>
2022-12-02 16:58:21 -0800

    Added functions to calculate profit per game and per season if you bet  on a single team to win each game

--- query.py
+++ query.py
@@ -13,7 +13,6 @@ from database import *
 
 #Calculate # games played per team per season
 def verifyGameCounts():
-	#First just calculating # of wins
 	res= cur.execute("""
 		SELECT team, seasonStartYear, MAX(gameNumber) AS gamesPlayed
 		FROM games
@@ -25,7 +24,6 @@ def verifyGameCounts():
 
 #Calculate # games won per team per season
 def verifyWinCounts():
-	#First just calculating # of wins
 	res= cur.execute("""
 		SELECT team, seasonStartYear, SUM(outcome) AS wins
 		FROM games
@@ -35,7 +33,61 @@ def verifyWinCounts():
 	return res.fetchall()
 
 
+#Calculate earnings if I bet $X on team T to Z[win/lose] each game of year Y
+#Starting with static T,X,Y,Z: 2021, $100, Celtics, Win
+def calculatePayoutPerGame():
+	res= cur.execute("""
+		WITH betResults
+		AS
+		(SELECT seasonStartYear, gameNumber, team, outcome, winOdds,
+			100 AS wager,
+			(SIGN(winOdds)+1)/2 AS isUnderdog,
+			1-((SIGN(winOdds)+1)/2) AS isFavorite
+		FROM games
+		)
+
+		SELECT seasonStartYear, gameNumber, team, outcome, winOdds, wager,
+			(outcome)*(isUnderdog*(wager/100)*(winOdds+100) + isFavorite*(wager/ABS(winOdds))*(100+ABS(winOdds))) AS payout,
+			(outcome)*(isUnderdog*(wager/100)*(winOdds+100) + isFavorite*(wager/ABS(winOdds))*(100+ABS(winOdds))) - wager AS profit
+		FROM betResults
+		WHERE team='Boston Celtics' AND seasonStartYear=2021
+		ORDER BY team ASC, gameNumber ASC;
+		""")
+	return res.fetchall()
+
+
+def calculatePayoutPerSeason():
+	res= cur.execute("""
+		WITH betResults
+		AS
+		(SELECT seasonStartYear, gameNumber, team, outcome, winOdds,
+			100 AS wager,
+			(SIGN(winOdds)+1)/2 AS isUnderdog,
+			1-((SIGN(winOdds)+1)/2) AS isFavorite
+		FROM games
+		),
+
+		payouts
+		AS 
+		(SELECT seasonStartYear, gameNumber, team, outcome, winOdds, wager,
+			(outcome)*(isUnderdog*(wager/100)*(winOdds+100) + isFavorite*(wager/ABS(winOdds))*(100+ABS(winOdds))) AS payout,
+			(outcome)*(isUnderdog*(wager/100)*(winOdds+100) + isFavorite*(wager/ABS(winOdds))*(100+ABS(winOdds))) - wager AS profit
+		FROM betResults
+		)
+
+		SELECT seasonStartYear, team, 
+			MAX(gameNumber) AS totalGames,
+			SUM(wager) AS totalWager,
+			ROUND(SUM(payout),2) AS totalPayout, 
+			ROUND(SUM(profit),2) AS totalProfit
+		FROM payouts
+		GROUP BY seasonStartYear, team
+		ORDER BY team ASC, seasonStartYear DESC;
+		""")
+	return res.fetchall()
+
+
 if __name__ == '__main__':
-	for r in verifyGameCounts():
+	for r in calculatePayoutPerSeason():
 		print(r)
 	con.close()
