5246d1507fa8902a372ba324db37740831407cdd
nikhilNathwani <nnathwani36@gmail.com>
2023-01-26 14:20:48 -0800

    Updated total-profit div so it now displays how much money I would have won. Doesn't update text to 'lost' if you actually lost money.

--- static/js/query-ui-update.js
+++ static/js/query-ui-update.js
@@ -17,39 +17,46 @@ function queryGames() {
   fetch(`/query?bet=${bet}&team=${team}&outcome=${outcome}&seasonStart=${seasonStartYear}`)
     .then(response => response.json())  
     .then(response => {
-      makeTotalProfitUI(response.profit);
-      makeRawDataUI(response.games);  
+      makeTotalProfitBox(response.profit);
+      makeRawDataBox(response.games);  
     });
 }
 
-function makeTotalProfitUI(profit) {
+function makeTotalProfitBox(profit) {
   // Set up the results container
-  const totalProfitUI = document.getElementById('total-profit-UI');
-  totalProfitUI.innerHTML = '';
+  const totalProfitBox = document.getElementById('total-profit-box');
+  totalProfitBox.innerHTML = '';
 
-  //Create the 'then you would have won' text
-  const caption= document.createElement('span');
-  caption.id = "caption-text";
-  caption.innerText = "Then you would have...";
-  totalProfitUI.appendChild(caption);
+  //Create the 'then you would have won' text, i.e. the "pre-text"
+  const preText= document.createElement('span');
+  preText.id = "pre-text";
+  preText.innerText = "Then you would have...";
+  totalProfitBox.appendChild(preText);
+
+  //Create the '[won/lost] [profit $ amount]' text, i.e. the "result-text"
+  const resultText= document.createElement('span');
+  resultText.id = "result-text";
+  resultText.innerText = "Won " + profit;
+  totalProfitBox.appendChild(resultText);
 
   return;
 }
 
-function makeRawDataUI(games) {
+
+function makeRawDataBox(games) {
   console.log(games);
 
   // Set up the results container
-  const rawDataUI = document.getElementById('raw-data-UI');
-  rawDataUI.innerHTML = '';
+  const rawDataBox = document.getElementById('raw-data-box');
+  rawDataBox.innerHTML = '';
 
   // Loop through the games and create a new element for each one
   games.forEach(game => {
     const gameElement = document.createElement('div');
     gameElement.innerHTML = `${game[0]} - ${game[1]} - ${game[2]} - ${game[3]} - ${game[4]} - ${game[5]}`;
-    rawDataUI.appendChild(gameElement);
+    rawDataBox.appendChild(gameElement);
   });
 
-  rawDataUI.appendChild(document.createElement('br'))
-  rawDataUI.appendChild(document.createElement('br'))
+  rawDataBox.appendChild(document.createElement('br'))
+  rawDataBox.appendChild(document.createElement('br'))
 }
